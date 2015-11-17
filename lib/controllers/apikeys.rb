20abef44c96c75c29000be8d85dfbfa3380191c8
Kevin Thompson <kevin.david.thompson@gmail.com>
2015-11-17 11:09:16 -0600

    adds form to create new api keys

--- lib/controllers/apikeys.rb
+++ lib/controllers/apikeys.rb
@@ -1,10 +1,12 @@
 namespace '/apikeys' do
   get do
-    @keys = APIKey.find user: current_user
+    @keys = APIKey.where user: current_user
     erb :"apikeys/index"
   end
 
   get '/new' do
+    @key = APIKey.new
+    @key.key = SecureRandom.uuid
     erb :"apikeys/new"
   end
 end
