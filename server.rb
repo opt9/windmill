cfed2209ab70356862c4ae9284bcea00cf78a51c
Kevin Thompson <kevin.david.thompson@gmail.com>
2015-11-20 14:38:02 -0600

    removes some temp code I used to get around authentication when I was on an airplane with no internet

--- server.rb
+++ server.rb
@@ -55,7 +55,6 @@ configure do
 end
 
 before do
-  session[:email] = "kdt@heroku.com"
   pass if request.path_info =~ /^\/auth\//
   pass if request.path_info =~ /^\/api\//
   pass if request.path_info =~ /^\/status/
